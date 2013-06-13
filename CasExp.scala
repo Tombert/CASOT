a77246cb05331eb8d28e1e97a361529ab1a588a7
Thomas Gebert <thomasgebert@gmail.com>
2013-06-13 10:54:46 -0500

    Update gitignore for more files that emacs generates. Add the basic class structure for teh CAS objects.

---
+++ CasExp.scala
@@ -0,0 +1,3 @@
+class CasExp extends CasObj {
+  
+}
---
+++ CasNum.scala
@@ -0,0 +1,3 @@
+class CasNum extends CasObj {
+  //This is going to be a generic superclass that every object in the repl will inherit from.  The reason we're making it is I think it'll be easier to append attributes this way. 
+}
