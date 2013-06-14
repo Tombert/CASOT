8112ff26c4fe967cd5fa543eed8dd1c1a648f863
Thomas Gebert <thomasgebert@gmail.com>
2013-06-14 07:03:16 -0500

    Right now the coefficients are just going to be ints to facilitate easy math.

--- CasObj.scala
+++ CasObj.scala
@@ -1,4 +1,5 @@
 trait CasObj {
+  var coef:Int;
   var myExp = new CasExp
   //This is going to be a generic superclass that every object in the repl will inherit from.  The reason we're making it is I think it'll be easier to append attributes this way. 
 
