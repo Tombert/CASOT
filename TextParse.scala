2931070073de3120a8fedae6abf3300d421b801f
Thomas Gebert <thomasgebert@gmail.com>
2013-06-17 12:05:30 -0500

    Make it so deserialize has a recursive sandwiching function to get a substring of the parentheses.  After this, we'll need something that checks if there are coefficients for the parentheses, and possibly put a possible expression object in the realExpression as it's not inconceivable to think that there would be parentheses inside other parentheses.

--- TextParse.scala
+++ TextParse.scala
@@ -14,8 +14,25 @@ object TextParse {
     }
   }
 
-  def deserialize(a:String) = {
-    
+  def deserialize(a:String):Unit = {
+    var beginParen:Char = ' '
+    var endParen:Char = ' '
+    var beginIndex:Int = 0
+    var endIndex:Int = a.length-1
+    for (i<- 0 to a.length-1){
+      if(beginParen != '(' && a(i) == '(') {
+	beginParen = a(i)
+	beginIndex = i
+      }
+      if(endParen != ')' && a(a.length-1-i)==')'){
+	endParen = a(a.length-1-i)
+	endIndex = a.length-1-i
+      }
+      if (beginParen == '(' && endParen == ')'){
+	var outerMostParen = a.substring(beginIndex, endIndex)
+	deserialize(outerMostParen)
+      }
+    }
     var objects = a.split('+')
     for(i<-objects) {
       println(i)
