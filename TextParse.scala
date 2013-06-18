c252915622cab900cf07afcde3ff77f26bc65e0b
Thomas Gebert <thomasgebert@gmail.com>
2013-06-18 13:03:02 -0500

    Change the text parsing in the repl.
    The problem is that if the user put something like (x+1)^(x+1), it would break
    everything.  The way I have it integrated now allows for better tokenization
    by adding a parenthese counter and making the functoin recursive.

--- TextParse.scala
+++ TextParse.scala
@@ -19,23 +19,38 @@ object TextParse {
     var endParen:Char = ' '
     var beginIndex:Int = 0
     var endIndex:Int = a.length-1
-    for (i<- 0 to a.length-1){
-      if(beginParen != '(' && a(i) == '(') {
-	beginParen = a(i)
-	beginIndex = i
+    var counter:Int = 0
+    for (i<-0 to a.length-1) {
+      if (a(i)=='('){
+	counter+=1
+	beginIndex = i+1
       }
-      if(endParen != ')' && a(a.length-1-i)==')'){
-	endParen = a(a.length-1-i)
-	endIndex = a.length-1-i
-      }
-      if (beginParen == '(' && endParen == ')'){
-	var outerMostParen = a.substring(beginIndex, endIndex)
-	deserialize(outerMostParen)
+      else if(a(i)==')'){
+	counter-=1
+	if(counter == 0){
+	  endIndex =  i-1
+	  deserialize(a.substring(beginIndex,endIndex))
+	}
       }
     }
-    var objects = a.split('+')
-    for(i<-objects) {
-      println(i)
-    }
+
+    // for (i<- 0 to a.length-1){
+    //   if(beginParen != '(' && a(i) == '(') {
+    // 	beginParen = a(i)
+    // 	beginIndex = i
+    //   }
+    //   if(endParen != ')' && a(a.length-1-i)==')'){
+    // 	endParen = a(a.length-1-i)
+    // 	endIndex = a.length-1-i
+    //   }
+    //   if (beginParen == '(' && endParen == ')'){
+    // 	var outerMostParen = a.substring(beginIndex, endIndex)
+    // 	deserialize(outerMostParen)
+    //   }
+    // }
+    // var objects = a.split('+')
+    // for(i<-objects) {
+    //   println(i)
+    // }
   }
 }
